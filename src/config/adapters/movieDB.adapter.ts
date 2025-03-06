d7ba079ed72532cf3760658a8cee40feb9ebf75c
percyzavala <percyzavala@gmail.com>
2025-03-06 00:14:44 -0500

    Se configuro .env  variable de entorno

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,11 +1,12 @@
 
+import { THE_MOVIE_DB_API_KEY } from '@env';
 import { AxiosAdapter } from './http/axios.adapter';
 import { FetchAdapter } from './http/fech.adapter';
 
 export const movieDBFetcher=new   AxiosAdapter({
   baseURL: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: 'ab8ed7d408763543df99341e3133a4af',
+    api_key: THE_MOVIE_DB_API_KEY ?? 'no-key',
     language: 'es',
     
   },
